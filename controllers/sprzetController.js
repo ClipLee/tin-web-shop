06cb9b3f51746fdfd9235b7bf456f9b7ec566430
LinDen <k.lipski777@gmail.com>
2023-01-24 16:18:51 +0100

    pierwszy kontroler dla sprzetu

---
+++ controllers/sprzetController.js
@@ -0,0 +1,11 @@
+exports.showSprzetList = (req, res, next) => {
+    res.render('pages/sprzet/sprzet', {}); //generowanie widoku na podstawie wybranego szablonu
+}
+
+exports.showAddSprzetForm = (req, res, next) => {
+    res.render('pages/sprzet/form-sprz', {});
+}
+
+exports.showSprzetDetails = (req, res, next) => {
+    res.render('pages/sprzet/list-sprz-details', {});
+}
---
+++ routes/sprzetRoute.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const router = express.Router();
+
+const employeeControler = require('../controllers/sprzetController');
