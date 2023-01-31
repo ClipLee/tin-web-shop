e52c4cab2eb1bb466bad1a234622052db99f31d4
LinDen <k.lipski777@gmail.com>
2023-01-31 02:24:05 +0100

    Views for /zamowienia

--- controllers/zamowieniaController.js
+++ controllers/zamowieniaController.js
@@ -12,6 +12,18 @@ exports.showAddZamowieniaForm = (req, res, next) => {
     });
 }
 
+exports.showEditZamowieniaForm = (req, res, next) => {
+    res.render('pages/zamowienia/form-edit', {
+        navLocation: 'zam'
+    });
+}
+
+exports.deleteZamowienia = (req, res, next) => {
+    res.render('pages/zamowienia/list-empty', {
+        navLocation: 'zam'
+    });
+}
+
 exports.showZamowieniaDetails = (req, res, next) => {
     res.render('pages/zamowienia/list-details', {
         navLocation: 'zam'
--- routes/zamowieniaRoute.js
+++ routes/zamowieniaRoute.js
@@ -5,6 +5,8 @@ const zamowieniaController = require('../controllers/zamowieniaController');
 
 router.get('/', zamowieniaController.showZamowieniaList);
 router.get('/add', zamowieniaController.showAddZamowieniaForm);
+router.get('/edit/:zamId', zamowieniaController.showEditZamowieniaForm);
 router.get('/details/:zamId', zamowieniaController.showZamowieniaDetails);
+router.get('/delete/:zamId', zamowieniaController.deleteZamowienia);
 
 module.exports = router;
