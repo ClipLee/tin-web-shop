aa8223bbcdc0bf532b6ec78e695fcd1d3d6e307c
LinDen <k.lipski777@gmail.com>
2023-02-01 22:47:34 +0100

    Sprzed do sprzedazy aka "zamowienia" sequelize

---
+++ config/sequelize/Sprzed_do_sprzedazy.js
@@ -0,0 +1,27 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../../config/sequelize/sequelize');
+
+const Sprzet_do_sprzedazy = sequelize.define('Sprzet_do_sprzedazy', {
+    id_sprzet_do_sprzedazy: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    Opakowanie_prezentowe: {
+        type: Sequelize.SMALLINT,
+        allowNull: false
+    },
+    Numer_seryjny: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    sprz_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    wys_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }
+});
