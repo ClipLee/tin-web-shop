d1eb16d8bf501efc1f6df984ff20d96d7b99f2ff
LinDen <k.lipski777@gmail.com>
2023-02-01 22:10:46 +0100

    Sequelize dla Sprzet

--- config/sequelize/Sprzet.js
+++ config/sequelize/Sprzet.js
@@ -1,6 +1,37 @@
 const Sequelize = require('sequelize');
 const sequelize = require('../../config/sequelize/sequelize');
 
-const Sprzet = sequelize.define('Employee', {
-    
-})
+const Sprzet = sequelize.define('Sprzet', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+    },
+    Producent: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    id_producenta: {
+        type: Sequelize.STRING,
+        allowNull: true,
+    },
+    Nazwa: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    Cena: {
+        type: Sequelize.DECIMAL(5,2),
+        allowNull: false,
+    },
+    Data_premiery: {
+        type: Sequelize. DATE,
+        allowNull: true;
+    },
+    Wymiary_opakowania: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+module.exports = Sprzet;
