d5559ab13b0317fdeefdfb56a511342a12df6735
LinDen <k.lipski777@gmail.com>
2023-02-01 23:47:56 +0100

    Connection of the new function to app.js

--- app.js
+++ app.js
@@ -41,4 +41,10 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
+const sequelizeInit = require('./config/sequelize/init');
+sequelizeInit()
+  .catch(err => {
+    console.log(err);
+  });
+
 module.exports = app;
