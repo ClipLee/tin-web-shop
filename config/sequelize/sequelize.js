d3159942a8cfc65dda5694dec0e6cd37e36d520e
LinDen <k.lipski777@gmail.com>
2023-02-02 04:30:16 +0100

    fix

--- config/sequelize/sequelize.js
+++ config/sequelize/sequelize.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('tin-projekt-lipski-s20901', `root`, `root`, {
+const sequelize = new Sequelize('tin-projekt-lipski-s20901', 'root', 'root', {
     dialect: 'mysql',
     host: 'localhost'
 });
